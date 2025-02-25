a334fcdd9b28a01abfd73b7a11f59dfb6c18a3e6
allen <ttom9264586@gmail.com>
2025-02-25 13:57:10 +0800

    Changes

--- Modules/Common/Services/BaseService.php
+++ Modules/Common/Services/BaseService.php
@@ -1169,7 +1169,7 @@ class BaseService
     {
         $http = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
 
-//        return $http.$_SERVER['HTTP_HOST'];
+        return $http.$_SERVER['HTTP_HOST'];
         return 'https://api1.49tkapi8.com';
     }
 
