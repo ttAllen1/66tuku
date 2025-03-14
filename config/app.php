d1452002263af3c48c5be3c3b12dd708f43a7124
allen <ttom9264586@gmail.com>
2025-03-14 14:44:49 +0800

    Changes

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         Mews\Captcha\CaptchaServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class
+//        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
