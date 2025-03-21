1640fb6e9d9dce9d12dc73c058313c79254bcb6d
allen <ttom9264586@gmail.com>
2025-03-21 18:45:28 +0800

    Changes

--- restart_services.sh
+++ restart_services.sh
@@ -5,6 +5,7 @@ pgrep -f "real-open" | xargs kill -9
 pgrep -f "queue:work" | xargs kill -9
 pgrep -f "forecast-bets" | xargs kill -9
 pgrep -f "zan-money" | xargs kill -9
+pgrep -f "index-guess" | xargs kill -9
 
 # 重新启动服务
 nohup php artisan module:real-open > /dev/null 2>&1 &
@@ -16,4 +17,5 @@ nohup php artisan module:forecast-bets-four > /dev/null 2>&1 &
 nohup php artisan module:forecast-bets-five > /dev/null 2>&1 &
 nohup php artisan module:forecast-bets-six > /dev/null 2>&1 &
 nohup php artisan module:forecast-bets-seven > /dev/null 2>&1 &
+nohup php artisan module:index-guess > /dev/null 2>&1 &
 nohup php artisan queue:work > /dev/null 2>&1 &
