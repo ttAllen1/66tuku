43fc56b333926bed7aa4ffa681fc753b9227c703
allen <ttom9264586@gmail.com>
2025-03-23 15:51:42 +0800

    Changes

--- Modules/Api/Models/IndexPic.php
+++ Modules/Api/Models/IndexPic.php
@@ -2,11 +2,24 @@
 
 namespace Modules\Api\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 class IndexPic extends BaseApiModel
 {
     public function picOther()
     {
 //        return $this->hasOne(YearPic::class, 'pictureTypeId', 'pictureTypeId')->where('year', date('Y'));
         return $this->hasOne(YearPic::class, 'pictureTypeId', 'pictureTypeId');
+    }
+
+    /**
+     * @return HasOne
+     */
+    public function picDetail()
+    {
+        return $this->hasOne(PicDetail::class, 'pictureTypeId', 'pictureTypeId')
+            ->where('year', date('Y'))
+            ->orderBy('issue', 'desc')
+            ->limit(1);
     }
 }
