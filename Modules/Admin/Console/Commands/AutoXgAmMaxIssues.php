974f578975184d70c32928c23baacb1c164c125b
allen <ttom9264586@gmail.com>
2025-04-10 19:53:52 +0800

    Changes

--- Modules/Admin/Console/Commands/AutoXgAmMaxIssues.php
+++ Modules/Admin/Console/Commands/AutoXgAmMaxIssues.php
@@ -17,21 +17,21 @@ class AutoXgAmMaxIssues extends Command
             'color'     => 1,
             'url'       => "https://49208.com/unite49/h5/index/search?year=2025&keyword=&color=1",
         ],
-        [
-            'type'      => 1,
-            'color'     => 2,
-            'url'       => "https://49208.com/unite49/h5/index/search?year=2025&keyword=&color=2",
-        ],
-        [
-            'type'      => 2,
-            'color'     => 1,
-            'url'       => "https://49208.com/unite49/h5/index/search?year=2025&keyword=&color=1",
-        ],
-        [
-            'type'      => 2,
-            'color'     => 2,
-            'url'       => "https://49208.com/unite49/h5/index/search?year=2025&keyword=&color=2",
-        ]
+//        [
+//            'type'      => 1,
+//            'color'     => 2,
+//            'url'       => "https://49208.com/unite49/h5/index/search?year=2025&keyword=&color=2",
+//        ],
+//        [
+//            'type'      => 2,
+//            'color'     => 1,
+//            'url'       => "https://49208.com/unite49/h5/index/search?year=2025&keyword=&color=1",
+//        ],
+//        [
+//            'type'      => 2,
+//            'color'     => 2,
+//            'url'       => "https://49208.com/unite49/h5/index/search?year=2025&keyword=&color=2",
+//        ]
     ];
 
     protected $signature = 'module:auto-xg-am-max-issue';
